57db68dcc4b66ff5b72301d1915b466caab84065
ashutosh <ashu.p89@gmail.com>
2021-07-13 20:45:18 +0530

    initial codebase setup with flyway

---
+++ src/test/java/com/ashu/amantestproject/AmantestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.amantestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmantestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
