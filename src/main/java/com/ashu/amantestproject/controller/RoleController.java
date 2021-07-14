6c32d5d571c3fa89f257a95af36a03ca96eeca70
ashutosh <ashu.p89@gmail.com>
2021-07-14 07:40:28 +0530

    swagger added

--- src/main/java/com/ashu/amantestproject/controller/RoleController.java
+++ src/main/java/com/ashu/amantestproject/controller/RoleController.java
@@ -4,6 +4,7 @@ import com.ashu.amantestproject.entity.RoleEntity;
 import com.ashu.amantestproject.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class RoleController {
     @Autowired
     RoleService roleService;
 
-    @RequestMapping("/roles")
+    @RequestMapping(path = "/roles", method = RequestMethod.GET)
     public List<RoleEntity> getAllRoles(){
         return roleService.getAllRoles();
     }
